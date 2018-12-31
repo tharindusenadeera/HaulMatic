ee481303ab14ad00678cce917ccdc51567a92c56
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-12-31 06:39:04 +0530

    table

--- haulmatic/src/App.js
+++ haulmatic/src/App.js
@@ -7,18 +7,21 @@ class App extends Component {
     return (
       <div className="App">
         <div className="list students">
-          <table>
-            <tr>
-              <td>Student ID</td>
-              <td>Student Name</td>
-              <td>Student Age</td>
-            </tr>
-            <tr>
-              <td>{}</td>
-              <td>{}</td>
-              <td>{}</td>
-            </tr>
-          </table>
+          <div className="col-sm-6">
+            <table>
+              <tr>
+                <td>Student ID</td>
+                <td>Student Name</td>
+                <td>Student Age</td>
+              </tr>
+              <tr>
+                <td>{}</td>
+                <td>{}</td>
+                <td>{}</td>
+              </tr>
+            </table>
+          </div>
+
         </div>
 
       </div>
