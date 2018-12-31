610166c30f1da4286e58df64a1f8b1a6f7563771
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-12-31 06:37:07 +0530

    changes in react app

--- haulmatic/src/App.js
+++ haulmatic/src/App.js
@@ -6,7 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        HaulMatic
+        <div className="list students">
+          <table>
+            <tr>
+              <td>Student ID</td>
+              <td>Student Name</td>
+              <td>Student Age</td>
+            </tr>
+            <tr>
+              <td>{}</td>
+              <td>{}</td>
+              <td>{}</td>
+            </tr>
+          </table>
+        </div>
 
       </div>
     );
