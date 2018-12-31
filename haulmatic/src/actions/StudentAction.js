977442f3b8d2caeed15719b956d608e7d5045213
tharindusenadeera <tharindusenadeera081@gmail.com>
2018-12-31 12:18:49 +0530

    reducers

--- haulmatic/src/actions/StudentAction.js
+++ haulmatic/src/actions/StudentAction.js
@@ -1,1 +1,16 @@
 //list students
+export const getStudents = () => {
+    return dispatch =>
+        axios
+            .get(process.env.REACT_APP_API_URL + "students", {
+                headers: {
+                    Authorization: localStorage.access_token
+                }
+            })
+            .then(response => {
+                dispatch({
+                    type: GET_STUDENTS,
+                    payload: response.data.make
+                });
+            });
+}
